e53528eb861984eddd5feb7b51a633ed52be5c63
焦品貴 <jpink0210@gmail.com>
2023-06-01 22:02:32 +0800

    [Middleware] 引用方式介紹

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// 這裡的 middleware 就是從 kernal 裡面拿出來用的，可以去找源頭
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
