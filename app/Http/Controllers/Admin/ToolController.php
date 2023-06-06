d4d9d0caefafe9ccd5075af01e106c9fc23f306f
焦品貴 <jpink0210@gmail.com>
2023-06-06 15:48:58 +0800

    [Queue] 3. 執行佇列server(long-lived)

--- app/Http/Controllers/Admin/ToolController.php
+++ app/Http/Controllers/Admin/ToolController.php
@@ -21,10 +21,21 @@ class ToolController extends Controller
                 把你想要的 job 灌入 job table(類似於代辦清單中)
             postman:
                 http://127.0.0.1:8000/admin/tools/updateProductPrice
+        */
+        /*
+            onQueue:
+                官方 queue:table 專用 operator
+                這個 table 有預設幫 jobs 做分類，onQueue 就是在塞工作的 設定分類
+                如果沒有設定，就是 default
+
+            執行佇列：
+                pa queue:work database --queue=default
+                pa queue:work database --queue=tool
 
         */
+
         foreach ($products as $product) {
-            UpdateProductPrice::dispatch($product);
+            UpdateProductPrice::dispatch($product)->onQueue('tool');
         }
     }
 }
