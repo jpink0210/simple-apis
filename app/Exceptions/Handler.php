51948ca74baf002183ee78bffb1238f79caf18be
焦品貴 <jpink0210@gmail.com>
2023-08-11 15:33:38 +0800

    recover Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,8 +4,6 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-use Illuminate\Auth\AuthenticationException;
-
 
 class Handler extends ExceptionHandler
 {
@@ -32,47 +30,12 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      *
-     * exception: 錯誤訊息的類別是什麼： get_class
-     * 這是內建 Exception 套件報錯回傳的地方
-     * 
      * @return void
      */
     public function register()
     {
-        $this->reportable(function (\Throwable $exception) {
-            $user = auth()->user();
-            LogError::create([
-                'user_id'    => $user ? $user->id : 0,
-                'message'   => $exception->getMessage(),
-                'exception' => get_class($exception),
-                'line'      => $exception->getLine(),
-                'trace'     => array_map(
-                    function ($trace) {
-                        unset($trace['args']);
-                        return $trace;
-                    },
-                    $exception->getTrace()
-                ),
-                'method'      => request()->getMethod(),
-                'params'      => request()->all(),
-                'uri'         => request()->getPathInfo(),
-                'user_agent'  => request()->userAgent(),
-                'header'      => request()->headers->all()
-            ]);
+        $this->reportable(function (Throwable $e) {
+            //
         });
-
-        // 直接製作錯誤畫面，並且導過去
-        // $this->renderable(function (\Exception $e) {
-        //     return response()->view('error', [], 500);
-        // });
-
-        // $this->reportable(function (Throwable $e) {
-        //     //
-        // });
-    }
-
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        return response()->json('Unauthenticated 失敗Ler', 401);
     }
 }
