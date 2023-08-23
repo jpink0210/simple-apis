772ffd65acc6539e1edc41879d2824cdb85905eb
焦品貴 <jpink0210@gmail.com>
2023-08-23 14:06:20 +0800

    [RWD] AdminPage Nav

--- resources/views/layouts/admin_nav.blade.php
+++ resources/views/layouts/admin_nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link active" href="/">商城首頁</a>
